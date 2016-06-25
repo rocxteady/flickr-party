fc00c105df8960a0fe8e7be0caca40b9e12f9977
Ulaş Sancak <ulas.san@gmail.com>
2016-06-25 21:04:40 +0300

    Last cell design started for party listing

---
+++ Flickr Party/PartyLastCell.h
@@ -0,0 +1,13 @@
+//
+//  PartyLastCell.h
+//  Flickr Party
+//
+//  Created by Ulaş Sancak on 24/06/16.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PartyLastCell : UICollectionViewCell
+
+@end
---
+++ Flickr Party/PartyLastCell.m
@@ -0,0 +1,13 @@
+//
+//  PartyLastCell.m
+//  Flickr Party
+//
+//  Created by Ulaş Sancak on 24/06/16.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import "PartyLastCell.h"
+
+@implementation PartyLastCell
+
+@end
