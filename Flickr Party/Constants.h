6625f609aee0afbcd97ef82f1921c5bf4d73077c
Ulaş Sancak <ulas.san@gmail.com>
2016-06-27 14:48:04 +0300

    Main color implemented.

--- Flickr Party/Constants.h
+++ Flickr Party/Constants.h
@@ -6,11 +6,15 @@
 //  Copyright © 2016 Ulaş Sancak. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 extern NSString *const FlickrAppKey;
 extern NSString *const FlickrAppSecret;
 
 @interface Constants : NSObject
+
+@property (strong, nonatomic, readonly) UIColor *maincolor;
+
++ (instancetype)sharedInstance;
 
 @end
