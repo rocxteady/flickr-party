53943acd77848dff0d0bf883f71610d542cfc587
Ulaş Sancak <ulas.san@gmail.com>
2016-06-27 14:48:44 +0300

    Main color implemented.

--- Flickr Party/Constants.m
+++ Flickr Party/Constants.m
@@ -11,6 +11,23 @@
 NSString *const FlickrAppKey = @"59b7b292d9279b71ea727d76bb06f9a7";
 NSString *const FlickrAppSecret = @"b7d6ff105f5e0f8b";
 
+static Constants *constants;
+
 @implementation Constants
+
++ (instancetype)sharedInstance {
+    if (!constants) {
+        constants = [[Constants alloc] init];
+    }
+    return constants;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _maincolor = [UIColor colorWithRed:0 green:122.0/255 blue:1.0 alpha:1.0];
+    }
+    return self;
+}
 
 @end
