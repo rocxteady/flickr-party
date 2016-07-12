94564c72dff02eb96f047f397dedb01cff0efc47
Ulaş Sancak <ulas.san@gmail.com>
2016-07-13 00:47:30 +0300

    Removed unnecessary FlickrAppSecret constant. Removed singleton and mainColor and secondColor properties.

--- Flickr Party/Constants.h
+++ Flickr Party/Constants.h
@@ -9,13 +9,7 @@
 #import <UIKit/UIKit.h>
 
 extern NSString *const FlickrAppKey;
-extern NSString *const FlickrAppSecret;
 
 @interface Constants : NSObject
-
-@property (strong, nonatomic, readonly) UIColor *maincolor;
-@property (strong, nonatomic, readonly) UIColor *secondColor;
-
-+ (instancetype)sharedInstance;
 
 @end
