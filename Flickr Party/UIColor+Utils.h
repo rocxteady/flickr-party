c94db4d4aab670221f377ab4aab99c234d6552cd
Ulaş Sancak <ulas.san@gmail.com>
2016-07-13 00:48:15 +0300

    Created for general app colors.

---
+++ Flickr Party/UIColor+Utils.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+Utils.h
+//  Flickr Party
+//
+//  Created by Ulaş Sancak on 12/07/16.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Utils)
+
++ (UIColor *)mainColor;
+
++ (UIColor *)secondColor;
+
+@end
---
+++ Flickr Party/UIColor+Utils.m
@@ -0,0 +1,21 @@
+//
+//  UIColor+Utils.m
+//  Flickr Party
+//
+//  Created by Ulaş Sancak on 12/07/16.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import "UIColor+Utils.h"
+
+@implementation UIColor (Utils)
+
++ (UIColor *)mainColor {
+    return [UIColor colorWithRed:0 green:122.0/255 blue:1.0 alpha:1.0];
+}
+
++ (UIColor *)secondColor {
+    return [UIColor colorWithRed:225.0/250 green:225.0/255 blue:225.0/255 alpha:1.0];
+}
+
+@end
