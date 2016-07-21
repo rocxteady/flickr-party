e5228c675fd0dcc0a2745250a7a0b7f36fa0fc86
Ulaş Sancak <ulas.san@gmail.com>
2016-07-21 19:44:21 +0300

    Getting URL query string from NSDictionary

---
+++ Flickr Party/NSDictionary+QueryString.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+QueryString.h
+//  FlickrParty
+//
+//  Created by Ulaş Sancak on 21/07/16.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (QueryString)
+
+- (NSString *)queryString;
+
+@end
---
+++ Flickr Party/NSDictionary+QueryString.m
@@ -0,0 +1,32 @@
+//
+//  NSDictionary+QueryString.m
+//  FlickrParty
+//
+//  Created by Ulaş Sancak on 21/07/16.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import "NSDictionary+QueryString.h"
+#import "NSString+URLEncodedString.h"
+
+@implementation NSDictionary (QueryString)
+
+- (NSString *)queryString {
+    NSString *queryString;
+    for (NSString *key in self.allKeys) {
+        NSString *value = self[key];
+        if (![value isKindOfClass:[NSString class]]) {
+            value = [NSString stringWithFormat:@"%@", value];
+        }
+        if (!queryString) {
+            queryString = @"?";
+        }
+        else {
+            queryString = [queryString stringByAppendingString:@"&"];
+        }
+        queryString = [NSString stringWithFormat:@"%@%@=%@", queryString, [NSString URLEscapeString:key], [NSString URLEscapeString:value]];
+    }
+    return queryString;
+}
+
+@end
